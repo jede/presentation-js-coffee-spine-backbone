a488f8ed3abf6dea0ce7d066fb6dfd92e72aae64
Ilia Mikhailov <ilia.mikhailov@gmail.com>
2011-11-15 23:47:14 +0100

    Working working

--- presentation.js
+++ presentation.js
@@ -1,6 +1,8 @@
 $(function() {
     $.deck('.slide');
     $.deck.defaults.codemirror.lineNumbers = false;
-
+    $(document).bind('deck.change', function(event, from, to) {
+       console.log('Moving from slide ' + from + ' to ' + to);
+});
 });
 
